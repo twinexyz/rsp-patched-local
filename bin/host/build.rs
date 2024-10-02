67ec6a179ac79ac6520df4b0b17814fa4096800d
parajuliswopnil <parajuliswopnil717@gmail.com>
2024-10-02 10:27:41 +0545

    added builder for local network

--- bin/host/build.rs
+++ bin/host/build.rs
@@ -13,4 +13,9 @@ fn main() {
         &format!("../{}", "client-linea"),
         BuildArgs { ignore_rust_version: true, ..Default::default() },
     );
+     build_program_with_args(
+        &format!("../{}", "client-local"),
+        BuildArgs { ignore_rust_version: true, ..Default::default() },
+    );
+
 }
