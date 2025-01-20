5d4ba94c6cac0f242a6e683275944f78ec3eeb19
parajuliswopnil <parajuliswopnil717@gmail.com>
2025-01-20 10:13:51 +0545

    chore: proof_file_name

--- bin/host/src/main.rs
+++ bin/host/src/main.rs
@@ -75,8 +75,8 @@ async fn main() -> eyre::Result<()> {
         args.block_number,
     )?;
 
-    let client_input = match (client_input_from_cache, provider_config.rpc_url) {
-        (Some(client_input_from_cache), _) => vec![client_input_from_cache],
+    let (client_input, to_block) = match (client_input_from_cache, provider_config.rpc_url) {
+        (Some(client_input_from_cache), _) => (vec![client_input_from_cache], args.to_block.unwrap()),
         (None, Some(rpc_url)) => {
             // Cache not found but we have RPC
             // Setup the provider.
@@ -116,7 +116,7 @@ async fn main() -> eyre::Result<()> {
                 bincode::serialize_into(&mut cache_file, &client_input)?;
             }
 
-            client_input
+            (client_input, to_block)
         }
         (None, None) => {
             eyre::bail!("cache not found and RPC URL not provided")
@@ -166,7 +166,7 @@ async fn main() -> eyre::Result<()> {
         }
 
         let proof_json = serde_json::to_string(&proof).unwrap();
-        let file_name = format!("{}/execution_proof_{}.proof", proof_dir, args.block_number);
+        let file_name = format!("{}/execution_proof_{}_{}.proof", proof_dir, args.block_number, to_block);
         let mut proof_file = File::create(&file_name).unwrap();
         proof_file.write_all(proof_json.as_bytes()).unwrap();
 
