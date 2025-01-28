8a89963b370f900db87cf6df64144f3ff066488d
parajuliswopnil <parajuliswopnil717@gmail.com>
2025-01-28 11:42:02 +0545

    fix: chain id

--- crates/executor/client/src/lib.rs
+++ crates/executor/client/src/lib.rs
@@ -161,7 +161,7 @@ impl ChainVariant {
                 Ok(Self::Linea(LineaVariant::new(rsp_primitives::chain_spec::linea_mainnet())))
             }
             CHAIN_ID_DEVNET => {
-                Ok(Self::Ethereum(EthereumVariant::new(rsp_primitives::chain_spec::devnet())))
+                Ok(Self::Devnet(DevnetVarient::new(rsp_primitives::chain_spec::devnet())))
             }
             _ => Err(ClientError::UnknownChainId(chain_id)),
         }
