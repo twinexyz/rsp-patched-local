a9164e1aa8e853b6eafeb18d3875bea6f35f739c
parajuliswopnil <parajuliswopnil717@gmail.com>
2025-01-28 09:14:45 +0545

    fix

--- bin/client-eth/src/main.rs
+++ bin/client-eth/src/main.rs
@@ -4,15 +4,17 @@ sp1_zkvm::entrypoint!(main);
 use rsp_client_executor::{io::ClientExecutorInput, ClientExecutor, EthereumVariant};
 
 pub fn main() {
-    // Read the input.
     let input = sp1_zkvm::io::read_vec();
-    let input = bincode::deserialize::<ClientExecutorInput>(&input).unwrap();
+    let input = bincode::deserialize::<Vec<ClientExecutorInput>>(&input).unwrap();
 
     // Execute the block.
     let executor = ClientExecutor;
-    let executor_output = executor.execute::<EthereumVariant>(input).expect("failed to execute client");
-    let block_hash = executor_output.block.hash_slow();
+    let mut executor_outputs = Vec::new();
+    for i in input {
+        let output = executor.execute::<EthereumVariant>(i).expect("failed to execute client");
+        executor_outputs.push(output);
+    }
 
     // Commit the block hash.
-    sp1_zkvm::io::commit(&block_hash);
+    // sp1_zkvm::io::commit(&block_hash);
 }
